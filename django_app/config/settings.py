ed9da25ba285dd76e88f37c3129928f9b9df4f37
joohyon <jhe0956@gmail.com>
2017-03-29 17:13:40 +0900

    model 작성완료

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        user = self.model(
+            username=username
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password):
+        user = self.model(
+            username=username
+        )
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    username = models.EmailField(unique=True, max_length=256)
+    nickname = models.CharField(max_length=256)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = [
+        'nickname',
+                       ]
+
+    def __str__(self):
+        return '{},{}'.format(
+            self.username,
+            self.nickname,
+        )
+
+    def get_full_name(self):
+        return '{},{}'.format(
+            self.nickname,
+            self.username
+        )
+
+    def get_short_name(self):
+        return self.nickname
