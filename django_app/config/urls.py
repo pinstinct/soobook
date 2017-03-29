6be41113008f8d31fe9fe6b36f3478937c66f48e
LimHM <gusals3407@gmail.com>
2017-03-30 00:33:43 +0900

    환경변수 설정, Static/Media path 설정, AWS설정

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from member.urls import apis as member_apis_urls
@@ -26,3 +28,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(api_urlpatterns, namespace='api')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
