f52b86a85d24a16145a92e4b68a50afd62d4d06c
LimHM <gusals3407@gmail.com>
2017-03-31 09:20:40 +0900

    URL 정리

--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from rest_framework.authtoken import views
 
 from .. import apis
 
 urlpatterns = [
-    url(r'^signup/$', apis.SignUpView.as_view()),
-    url(r'^profile/$', apis.ProfileView.as_view()),
-    url(r'^token-auth/', views.obtain_auth_token),
+    # url(r'^signup/$', apis.SignUpView.as_view()),
+    url(r'^signup/$', apis.SignUp.as_view()),
+    url(r'^login/$', apis.Login.as_view()),
+    # url(r'^profile/$', apis.ProfileView.as_view()),
+    # url(r'^token-auth/', views.obtain_auth_token),
 ]
