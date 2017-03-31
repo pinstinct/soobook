134eac2be15988af042ca2039cb723b0cfa8009d
LimHM <gusals3407@gmail.com>
2017-03-31 18:06:30 +0900

    logout API URL 설정

--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -3,9 +3,7 @@ from django.conf.urls import url
 from .. import apis
 
 urlpatterns = [
-    # url(r'^signup/$', apis.SignUpView.as_view()),
     url(r'^signup/$', apis.SignUp.as_view()),
     url(r'^login/$', apis.Login.as_view()),
-    # url(r'^profile/$', apis.ProfileView.as_view()),
-    # url(r'^token-auth/', views.obtain_auth_token),
+    url(r'^logout/$', apis.Logout.as_view()),
 ]
