7fdc1567a4f77153e6476ea57d761bdb0689b823
LimHM <gusals3407@gmail.com>
2017-03-31 09:23:06 +0900

    APIView를 이용하도록 SignUp API 변경

--- django_app/member/apis/user.py
+++ django_app/member/apis/user.py
@@ -1,27 +1,50 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from member.serializers import SignUpSerializer, LoginSerializer
+from member.serializers import SignUpSerializer
 
 __all__ = (
-    'SignUpView',
-    'ProfileView',
+    # 'SignUpView',
+    # 'ProfileView',
+    'SignUp',
+    'Login',
 )
 
 User = get_user_model()
 
 
-class SignUpView(generics.CreateAPIView):
-    serializer_class = SignUpSerializer
+# class SignUpView(generics.CreateAPIView):
+#     serializer_class = SignUpSerializer
 
 
-class ProfileView(APIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class SignUp(APIView):
+    def post(self, request, format=None):
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        serializer = LoginSerializer(user)
-        return Response(serializer.data)
+
+class Login(APIView):
+    # permission_classes = (IsAuthenticated,)
+
+
+    def post(self, request, format=None):
+        content = {
+            'user': request.user,
+            'auth': request.auth,
+        }
+        print(content)
+        return Response(content)
+
+# class ProfileView(APIView):
+#     permission_classes = (permissions.IsAuthenticated,)
+#
+#     def get(self, request, *args, **kwargs):
+#         user = request.user
+#         serializer = LoginSerializer(user)
+#         return Response(serializer.data)
