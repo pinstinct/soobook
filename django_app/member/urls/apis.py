356216a67889dc95b216dea6fbd83bcb982c05b6
sshong1991 <sshong1991@gmail.com>
2017-04-01 17:24:19 +0900

    url name 추가

--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .. import apis
 
 urlpatterns = [
-    url(r'^signup/$', apis.SignUp.as_view()),
-    url(r'^login/$', apis.Login.as_view()),
-    url(r'^logout/$', apis.Logout.as_view()),
+    url(r'^signup/$', apis.SignUp.as_view(), name='signup'),
+    url(r'^login/$', apis.Login.as_view(), name='login'),
+    url(r'^logout/$', apis.Logout.as_view(), name='logout'),
 ]
