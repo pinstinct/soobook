1873f0048a36e2997fc427e8eee175e4c68840b9
LimHM <gusals3407@gmail.com>
2017-04-03 16:26:19 +0900

    Book 모델 추가

--- django_app/book/models.py
+++ django_app/book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=200)
+    cover_thumbnail = models.URLField()
+    publisher = models.CharField(max_length=100)
+    description = models.TextField()
+    updated_date = models.DateTimeField(auto_now=True)
+    google_id = models.CharField(max_length=100)
