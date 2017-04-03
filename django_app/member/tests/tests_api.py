3c38f9e7f0f751420acea506806f3ea7961db124
sshong1991 <sshong1991@gmail.com>
2017-04-03 15:38:49 +0900

    requirements.txt 수정

--- django_app/member/tests/tests_api.py
+++ django_app/member/tests/tests_api.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APILiveServerTestCase
+# from passlib.hash import pbkdf2_sha256
 
 User = get_user_model()
 
@@ -14,10 +15,23 @@ class TestUser(APITestCase, APILiveServerTestCase):
             'nickname': 'testnick',
             'password': 'testpassword'
         }
+        # hash_password = pbkdf2_sha256.using(rounds=30000, salt_size=8).hash("testpassword")
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
 
         self.assertEqual(User.objects.get().username, 'test@test.com')
         self.assertEqual(User.objects.get().nickname, 'testnick')
-        self.assertEqual(User.objects.get().password, 'testpassword')
+        # self.assertEqual(User.objects.get().password, hash_password)
+
+    def test_login(self):
+        url = reverse('api:signup')
+        data = {
+            'username': 'test@teat.com',
+            'password': 'testpassword',
+        }
+        self.client.post(url, data, format='json')
+
+        url = reverse('member')
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
