d47fefc9b287cb07b0531c5b2c68020e74b2db67
LimHM <gusals3407@gmail.com>
2017-04-05 00:39:23 +0900

    book의 description필드 제약 추가

--- django_app/book/models.py
+++ django_app/book/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 
 
-# User = get_user_model()
-
-
 class Book(models.Model):
+    google_id = models.CharField(max_length=100)
     title = models.CharField(max_length=256)
     author = models.CharField(max_length=200)
     cover_thumbnail = models.URLField()
     publisher = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     updated_date = models.DateTimeField(auto_now=True)
-    google_id = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
-
-# class MyBookShelf(models.Model):
-#     my_book = models.ForeignKey(Book)
-#     user = models.ForeignKey(User)
-#     created_date = models.DateTimeField(auto_now_add=True)
