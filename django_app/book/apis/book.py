6281b38320c74844768852c804b550e583e39a5a
LimHM <gusals3407@gmail.com>
2017-04-05 15:37:54 +0900

    serializers 및 api 파일 추가

---
+++ django_app/book/apis/book.py
@@ -0,0 +1,24 @@
+from rest_framework.views import APIView
+
+__all__ = (
+    'Search',
+    'MyBook',
+)
+
+
+# 주현님
+class Search(APIView):
+    def get(self):
+        pass
+
+
+# 성수님
+class MyBook(APIView):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
