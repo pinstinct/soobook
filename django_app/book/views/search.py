3af0343dcddef79a125f3bb5df748d1e4d945bcd
joohyon <jhe0956@gmail.com>
2017-04-05 11:59:37 +0900

    google_book에서 불러오기

--- django_app/book/views/search.py
+++ django_app/book/views/search.py
@@ -6,10 +6,14 @@ from django.shortcuts import render
 
 from django_app.config.settings import config
 
+__all__ = (
+    'search_from_google',
+    'search'
+)
 google_api_key = config['google_book']['API_KEY']
 
 
-def search_from_google(keyword):
+def search_from_google(keyword, ):
     params = {
         'q': keyword,
         'key': google_api_key,
@@ -18,7 +22,6 @@ def search_from_google(keyword):
     r = requests.get('https://www.googleapis.com/books/v1/volumes', params=params)
     result = r.text
 
-    # 4. 해당 내용을 다시 json.loads()를 이용해 파이썬 객체로 변환
     result_dict = json.loads(result)
     return result_dict
 
@@ -32,23 +35,18 @@ def search(request):
     keyword = request.GET.get('keyword', '').strip()
 
     if keyword != '':
-        # 검색 결과를 받아옴
-        search_result = search_from_google(keyword)
 
-        # 검색결과에서 이전/다음 토큰, 전체 결과 개수를 가져와
-        # 템플릿에 전달할 context객체에 할당
+        search_result = search_from_google(keyword)
 
         total_items = search_result.get('totalItems')
 
         context['totalItems'] = total_items
         context['keyword'] = keyword
 
-        # 검색결과에서 'items'키를 갖는 list를 items변수에 할당 후 loop
         items = search_result['items']
         for item in items:
             published_date_str = item['volumeInfo']['publishedDate']
 
-            # 실제로 사용할 데이터
             google_id = item['id']
             title = item['volumeInfo']['title']
             authors = item['volumInfo']['authors']
@@ -56,10 +54,7 @@ def search(request):
             publisher = item['voulumeInfo']['publisher']
             published_date = parse(published_date_str)
             cover_thumbnail = item['volumeInfo']['imageLinks']['thumbnail']
-            # 이미 북마크에 추가된 영상인지 판단
 
-
-            # 현재 item을 dict로 정리
             cur_item_dict = {
                 'title': title,
                 'description': description,
@@ -71,4 +66,4 @@ def search(request):
             }
             book.append(cur_item_dict)
 
-    return render(request, 'video/search.html', context)
+    return render(request, 'book/index.html', context)
