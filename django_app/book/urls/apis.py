c25f24a863f9d81f6a44a80b4fd55b295003be46
LimHM <gusals3407@gmail.com>
2017-04-05 15:37:27 +0900

    url 연결

---
+++ django_app/book/urls/apis.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from .. import apis
+
+urlpatterns = [
+    url(r'^search/$', apis.Search.as_view(), name='search'),
+    url(r'^mybook/$', apis.MyBook.as_view(), name='mybook'),
+]
