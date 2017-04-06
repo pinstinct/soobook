cdf3f2eabb0b8690b864a0f598a120c1ed47f1d2
joohyon <jhe0956@gmail.com>
2017-04-06 14:34:37 +0900

    Search API 작성

--- django_app/book/apis/book.py
+++ django_app/book/apis/book.py
@@ -1,4 +1,8 @@
+from rest_framework import generics
 from rest_framework.views import APIView
+
+from book.models import Book
+from book.serializers import SearchSerializer
 
 __all__ = (
     'Search',
@@ -7,9 +11,15 @@ __all__ = (
 
 
 # 주현님
-class Search(APIView):
-    def get(self):
-        pass
+class Search(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = SearchSerializer
+
+    def get_queryset(self, **kwargs):
+        # project_id may be None
+        return self.queryset \
+            .filter(title=self.kwargs.get('title')) \
+            .filter(author=self.kwargs.get('author'))
 
 
 # 성수님
--- django_app/book/serializers.py
+++ django_app/book/serializers.py
@@ -1,4 +1,27 @@
 # 주현님
+from rest_framework import serializers
+
+from book.models import Book
+
+
+class SearchSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=200)
+    author = serializers.CharField(max_length=200)
+    cover_thumbnail = serializers.URLField()
+    publisher = serializers.CharField(max_length=100)
+    description = serializers.CharField(max_length=1000)
+
+    def create(self, *args, **kwargs):
+        pass
+
+    class meta:
+        fields = [
+            'title',
+            'author',
+            'cover_thumbnail',
+            'publisher',
+            'description',
+        ]
 
 
 
@@ -8,4 +31,8 @@
 
 
 
-# 성수님
+
+
+
+
+        # 성수님
