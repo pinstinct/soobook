4e50278d2084dd66188d94d4c631ef2e2111b4da
sshong1991 <sshong1991@gmail.com>
2017-04-07 15:22:12 +0900

    book list, add, delete api 구현중

--- django_app/book/apis/book.py
+++ django_app/book/apis/book.py
@@ -1,9 +1,19 @@
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from book.models import Book
+from book.serializers import MyBookSerializer
 
 __all__ = (
     'Search',
     'MyBook',
 )
+
+User = get_user_model()
 
 
 # 주현님
@@ -14,11 +24,20 @@ class Search(APIView):
 
 # 성수님
 class MyBook(APIView):
-    def get(self):
-        pass
+    def get(self, request):
+        mybook = Book.objects.filter(myuser=request.user.pk)
+        serializer = MyBookSerializer(mybook)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self):
-        pass
+    def post(self, request):
+        serializer = MyBookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self):
-        pass
+    def delete(self, request):
+        mybook = Book.objects.filter(myuser=request.user.pk)
+        mybook.delete()
+        return Response(status=status.HTTP_200_OK)
+
--- django_app/book/serializers.py
+++ django_app/book/serializers.py
@@ -9,3 +9,23 @@
 
 
 # 성수님
+
+from rest_framework import serializers
+
+from book.models import Book
+from member.models import MyUser
+
+
+class MyBookSerializer(serializers.ModelSerializer):
+    mybook = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), write_only=True)
+
+    class Meta:
+        model = Book
+        fields = (
+            'mybook',
+            'title',
+            'author',
+            'cover_thumbnail',
+            'publisher',
+            'description',
+        )
