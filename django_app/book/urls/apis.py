d9a3c2753c1ee929e1aeefcfcb74fd4652699208
LimHM <gusals3407@gmail.com>
2017-04-11 10:45:33 +0900

    mybook detail 및 search URL 추가

--- django_app/book/urls/apis.py
+++ django_app/book/urls/apis.py
@@ -5,6 +5,8 @@ from .. import apis
 urlpatterns = [
     url(r'^search/$', apis.Search.as_view(), name='search'),
     url(r'^mybook/$', apis.MyBook.as_view(), name='mybook'),
+    url(r'^mybook/detail/$', apis.MyBookDetail.as_view(), name='mybook_detail'),
+    url(r'^mybook/search/$', apis.MyBookSearch.as_view(), name='mybook_search'),
     url(r'^comment/$', apis.Comment.as_view(), name='comment'),
     url(r'^star/$', apis.Star.as_view(), name='star'),
 ]
