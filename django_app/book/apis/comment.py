b01edb5dd60d95eb8d27ab491c6d95584d466c85
LimHM <gusals3407@gmail.com>
2017-04-14 23:29:20 +0900

    Comment add, updqte 구현

--- django_app/book/apis/comment.py
+++ django_app/book/apis/comment.py
@@ -1,9 +1,28 @@
-from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+
+from book.models import BookComment
+from book.serializer import BookCommentSerializer
 
 __all__ = (
     'Comment',
 )
 
 
-class Comment(APIView):
-    pass
+class Comment(generics.GenericAPIView):
+    queryset = BookComment.objects.all()
+    serializer_class = BookCommentSerializer
+
+    def post(self, request):
+        mybook_id = self.request.data.get('mybook_id')
+        comment = self.request.data.get('comment')
+        defaults = {
+            'comment': comment
+        }
+        q, _ = BookComment.objects.update_or_create(
+            mybook_id=mybook_id,
+            defaults=defaults
+        )
+
+        serializer = self.get_serializer(q)
+        return Response(serializer.data)
