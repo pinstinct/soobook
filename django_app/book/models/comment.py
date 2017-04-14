461721e95b2874557ae5d7bfe5f083e1cf53e91d
LimHM <gusals3407@gmail.com>
2017-04-15 00:42:31 +0900

    related_name 추가

--- django_app/book/models/comment.py
+++ django_app/book/models/comment.py
@@ -6,4 +6,7 @@ from book.models import MyBook
 class BookComment(models.Model):
     comment = models.TextField()
     updated_date = models.DateTimeField(auto_now=True)
-    mybook = models.ForeignKey(MyBook)
+    mybook = models.ForeignKey(MyBook, related_name='comment')
+
+    def __str__(self):
+        return self.comment
