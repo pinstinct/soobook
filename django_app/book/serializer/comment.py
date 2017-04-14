1bd28fc829ac815662ab4f251d91bf210faa52bd
LimHM <gusals3407@gmail.com>
2017-04-14 23:29:20 +0900

    Comment add, updqte 구현

---
+++ django_app/book/serializer/comment.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from book.models import BookComment
+
+__all__ = (
+    'BookCommentSerializer',
+)
+
+
+class BookCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookComment
+        fields = (
+            'id',
+            'comment',
+            'updated_date',
+        )
