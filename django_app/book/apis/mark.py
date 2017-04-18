5ff2073211767ae7b1375850116bd7f63524a941
LimHM <gusals3407@gmail.com>
2017-04-18 16:29:47 +0900

    bookmark add API 예외처리

--- django_app/book/apis/mark.py
+++ django_app/book/apis/mark.py
@@ -1,7 +1,8 @@
+from rest_framework import exceptions
 from rest_framework import generics
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from rest_framework.settings import api_settings
 
 from book.models import BookMark
 from book.models import MyBook
@@ -15,31 +16,30 @@ __all__ = (
 class Mark(generics.GenericAPIView):
     queryset = BookMark.objects.all()
     serializer_class = BookMarkSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def post(self, request):
-        # user = self.request.user
-        mybook_id = self.request.data.get('mybook_id')
-        content = self.request.data.get('content')
-
-        # try:
-        # mybook = MyBook.objects.filter(user=user).filter(book=book).get()
-        mybook = MyBook.objects.get(id=mybook_id)
-        q, _ = BookMark.objects.get_or_create(
-            mybook=mybook,
-            content=content,
-        )
-
-        serializer = self.get_serializer(q)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # except:
-        #     # book.models.book.DoesNotExist: MyBookmatching query does not exist.
-        #     pass
-
-    def soobook_exception(self, data):
-        try:
-            return {'Location': data[api_settings.URL_FIELD_NAME]}
-        except (TypeError, KeyError):
-            return {}
+        data = self.request.data.keys()
+        if data:
+            mybook_id = self.request.data.get('mybook_id', '')
+            content = self.request.data.get('content', '')
+            if mybook_id and content != '':
+                try:
+                    mybook = MyBook.objects.get(id=mybook_id)
+                    q, _ = BookMark.objects.get_or_create(
+                        mybook=mybook,
+                        content=content,
+                    )
+                    serializer = self.get_serializer(q)
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                except:
+                    raise exceptions.ParseError({"ios_error_code": 4004, "detail": "Invalid mybook_id."})
+            else:
+                raise exceptions.ParseError({"ios_error_code": 4003,
+                                             "detail": ["'mybook_id', 'content' field may not be blank."]})
+        else:
+            raise exceptions.ParseError({"ios_error_code": 4002,
+                                         "detail": ["'mybook_id', 'content' field is required."]})
 
     def put(self, request):
         bookmakr_id = self.request.data.get('mark_id')
