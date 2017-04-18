290d537725e2ecbddb9c1fadeb2388e9d4fc5b4f
sshong1991 <sshong1991@gmail.com>
2017-04-18 17:52:58 +0900

    book api test complete

--- django_app/book/tests/tests_book_api.py
+++ django_app/book/tests/tests_book_api.py
@@ -1,35 +1,15 @@
+from django.urls import NoReverseMatch
+from django.urls import resolve
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APILiveServerTestCase, APIClient
+
+from book.models import MyBook
 from utils.auth_test import APIAuthMixin
+from utils.book_test import APIBookMixin
 
-from book.models import Book, MyBook
 
-
-class MyBookAPITestCase(APILiveServerTestCase, APIAuthMixin):  # from django.contrib.auth import get_user_model
-    @classmethod
-    def create_dummy_book(cls, dummy_title=None):
-        dummy_data = {
-            'google_id': 'dummy_google_id',
-            'title': dummy_title if dummy_title else 'dummy_title',
-            'author': 'dummy_author',
-            'cover_thumbnail': 'dummy_cover_thumbnail',
-            'publisher': 'dummy_publisher',
-            'description': 'dummy_description',
-            'keyword': 'dummy_keyword',
-        }
-        dummy_book = Book(**dummy_data)
-        dummy_book.save()
-        return dummy_book
-
-    @classmethod
-    def create_dummy_book_list(cls, num_of_books):
-        dummy_book_list = []
-        for i in range(num_of_books):
-            dummy_book = cls.create_dummy_book('dummy_title_{}'.format(i))
-            dummy_book_list.append(dummy_book)
-        return dummy_book_list
-
+class MyBookAPITestCase(APILiveServerTestCase, APIAuthMixin, APIBookMixin):
     def setUp(self):
         self.token = self.get_token(self.client)
         self.login_client = APIClient()
@@ -124,67 +104,21 @@ class MyBookAPITestCase(APILiveServerTestCase, APIAuthMixin):  # from django.con
         # self.assertEqual(dummy_book.pk, response.data[0])
         # print(response.data)
 
-# from django.urls import NoReverseMatch
-# from django.urls import resolve
-# from django.urls import reverse
-# from rest_framework import status
-# from rest_framework.test import APILiveServerTestCase, APIClient
-#
-# from utils.auth_test import APIAuthMixin
-#
-# User = get_user_model()
-#
-#
-# class MyBookAPITestCase(APILiveServerTestCase, APIAuthMixin):
-#     def test_apis_url_exists(self):
-#         try:
-#             resolve('/api/book/search/')
-#             resolve('/api/book/mybook/')
-#         except NoReverseMatch as e:
-#             self.fail(e)
-#
-#     def test_book_search(self):
-#         keyword = 'python'
-#         book_search_url = reverse('api:search')
-#         url = '{}?keyword={}'.format(
-#             book_search_url,
-#             keyword
-#         )
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_book_add(self):
-#         token = self.get_token(self.client)
-#         client = APIClient()
-#         client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-#         book_id = 300
-#         mybook_add_url = reverse('api:mybook')
-#         data = {
-#             'book_id': book_id
-#         }
-#         response = client.post(mybook_add_url, data)
-#
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#
-#     def test_book_delete(self):
-#         token = self.get_token(self.client)
-#         client = APIClient()
-#         client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-#         book_id = 300
-#         mybook_add_url = reverse('api:mybook')
-#         data = {
-#             'book_id': book_id
-#         }
-#         response = client.delete(mybook_add_url, data)
-#
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#     def test_book_list(self):
-#         userid = 21
-#         mybook_add_url = reverse('api:mybook')
-#         url = '{}?userid={}'.format(
-#             mybook_add_url,
-#             userid
-#         )
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class MyBookSearchAPITestCase(APILiveServerTestCase, APIAuthMixin):
+    def test_apis_url_exists(self):
+        try:
+            resolve('/api/book/search/')
+            resolve('/api/book/mybook/')
+        except NoReverseMatch as e:
+            self.fail(e)
+
+    def test_book_search(self):
+        keyword = 'python'
+        book_search_url = reverse('api:search')
+        url = '{}?keyword={}'.format(
+            book_search_url,
+            keyword
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
---
+++ django_app/utils/book_test.py
@@ -0,0 +1,26 @@
+from book.models import Book, MyBook
+
+
+class APIBookMixin(object):
+    @classmethod
+    def create_dummy_book(cls, dummy_title=None):
+        dummy_data = {
+            'google_id': 'dummy_google_id',
+            'title': dummy_title if dummy_title else 'dummy_title',
+            'author': 'dummy_author',
+            'cover_thumbnail': 'dummy_cover_thumbnail',
+            'publisher': 'dummy_publisher',
+            'description': 'dummy_description',
+            'keyword': 'dummy_keyword',
+        }
+        dummy_book = Book(**dummy_data)
+        dummy_book.save()
+        return dummy_book
+
+    @classmethod
+    def create_dummy_book_list(cls, num_of_books):
+        dummy_book_list = []
+        for i in range(num_of_books):
+            dummy_book = cls.create_dummy_book('dummy_title_{}'.format(i))
+            dummy_book_list.append(dummy_book)
+        return dummy_book_list
